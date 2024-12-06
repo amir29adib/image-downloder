13798b8ade42ae21af62770a2d88c3d91a279dab
Amir <amir29adib1234@gmail.com>
2024-12-06 08:36:39 +0330

    generate auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StatusModule } from './status/status.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [StatusModule, UserModule],
+  imports: [StatusModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

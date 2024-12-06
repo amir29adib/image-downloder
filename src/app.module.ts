467e6165a92689595cb7773a82a5963ca0f6b663
Amir <amir29adib1234@gmail.com>
2024-12-06 08:33:39 +0330

    generate user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StatusModule } from './status/status.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [StatusModule],
+  imports: [StatusModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
